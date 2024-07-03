5668f1e41fc687c5dc310416fba8223830af9d89
notZairus <notzairus@gmail.com>
2024-07-03 22:25:57 +0700

    will finish tomorrow

---
+++ src/js/home.js
@@ -0,0 +1,7 @@
+
+export function attachHome() {
+  let home = document.createElement('div');
+  home.id = 'home';
+  return home;
+}
+
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import '../style/main.css';
+import { attachHome } from './home';
+
+let content = document.getElementById('content');
+
+content.appendChild();
